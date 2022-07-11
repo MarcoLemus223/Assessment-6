14a06b34be316f36d62499b95d5b3172c3bda84c
Marco Lemus <marcoantonio_lemus@yahoo.com>
2022-07-11 12:51:23 -0600

    fixed assessment

--- functions.test.ts
+++ functions.test.ts
@@ -1,20 +1,19 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-    test('returning a array', async () => {
-        return 3
-    })
-})
+const testArr1 = [10, 20, 30, 40]
+const testArr2 = []
+const testArr3 = ['winter', 'summer', 'fall']
 
 describe('shuffleArray should', () => {
-    test('return name', async () => {
-        return 'Marco'
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(testArr1))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr2))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr3))).toBe(true)
     })
-})
 
-describe('shuffArray should', () => {
-    test('add 1 and 3', async () => {
-        return 4
+    test('have its output array have the smae length as its input', () => {
+        expect(shuffleArray(testArr1)).toHaveLength(testArr1.length)
+        expect(shuffleArray(testArr2)).toHaveLength(testArr2.length)
+        expect(shuffleArray(testArr3)).toHaveLength(testArr3.length)
     })
 })
